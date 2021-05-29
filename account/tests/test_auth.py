60964082094a7cd1b4e874b45ac3f361c3d4d9fe
ShrooqAyman <shayman@students.iugaza.edu.ps>
2021-05-29 11:45:48 +0300

    test

--- account/tests/test_auth.py
+++ account/tests/test_auth.py
@@ -5,11 +5,57 @@ from django.urls import reverse
 class BaseTest(TestCase):
     def setUp(self):
         self.register_url=reverse('register')
-         
+        self.login_url=reverse('login')
+        self.user={
+            'email':'testemail@gmail.com',
+            'username':'username',
+            'password':'password',
+            'password2':'password'
+        }
+        self.user_short_password={
+            'email':'testemail@gmail.com',
+            'username':'username',
+            'password':'tes',
+            'password2':'tes'
+        }
 
+        self.user_unmatching_password={
+
+            'email':'testemail@gmail.com',
+            'username':'username',
+            'password':'teslatt',
+            'password2':'teslatto'
+                    }
+
+        self.user_invalid_email={
+            
+            'email':'test.com',
+            'username':'username',
+            'password':'teslatt',
+            'password2':'teslatto'        }
+        return super().setUp()
 
 class RegisterTest(BaseTest):
    def test_can_view_page_correctly(self):
        response=self.client.get(self.register_url)
        self.assertEqual(response.status_code,200)
        self.assertTemplateUsed(response,'auth/register.html')
+
+   def test_can_register_user(self):
+        response=self.client.post(self.register_url,self.user,format='text/html')
+        self.assertEqual(response.status_code,302)
+
+   def test_cant_register_user_withshortpassword(self):
+        response=self.client.post(self.register_url,self.user_short_password,format='text/html')
+        self.assertEqual(response.status_code,200)
+   def test_cant_register_user_with_unmatching_passwords(self):
+        response=self.client.post(self.register_url,self.user_unmatching_password,format='text/html')
+        self.assertEqual(response.status_code,200)
+   def test_cant_register_user_with_invalid_email(self):
+        response=self.client.post(self.register_url,self.user_invalid_email,format='text/html')
+        self.assertEqual(response.status_code,200)
+
+   def test_cant_register_user_with_taken_email(self):
+        self.client.post(self.register_url,self.user,format='text/html')
+        response=self.client.post(self.register_url,self.user,format='text/html')
+        self.assertEqual(response.status_code,200)
