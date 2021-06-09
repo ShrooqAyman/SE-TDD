00f96a81a726f4a5f6c594eb9d8b6ca2696ff02d
ShrooqAyman <shayman@students.iugaza.edu.ps>
2021-06-09 13:27:46 +0300

    add new units test

--- account/tests/test_auth.py
+++ account/tests/test_auth.py
@@ -1,12 +1,18 @@
 from django.test import TestCase
+from django.test.client import Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+# to make tests success just uncomment urls in auth/urls.py
 
+# ************** Test Cases ***********************
 class BaseTest(TestCase):
     def setUp(self):
+        self.client =Client()
         self.register_url=reverse('register')
         self.login_url=reverse('login')
+        self.issue_url = reverse('issue')
+        self.resetPassword_url = reverse('resetPassword')
         self.user={
             'email':'testemail@gmail.com',
             'username':'username',
@@ -33,9 +39,69 @@ class BaseTest(TestCase):
             'email':'test.com',
             'username':'username',
             'password':'teslatt',
-            'password2':'teslatto'        }
+            'password2':'teslatto' }
+
+        self.not_valid_issue_date = {
+            'isbn' :"123",
+            'username' :'user1',
+            'issue_date' : '05/01/2021',
+            'issue_for' : '04/01/2021'
+        }
+
+        self.issue_with_empty_issue_date = {
+            'isbn': "123",
+            'username': 'user1',
+            'issue_date': '',
+            'issue_for': '04/01/2021'
+        }
+
+        self.issue_with_empty_issue_for = {
+            'isbn': "123",
+            'username': 'user1',
+            'issue_date': '05/01/2021',
+            'issue_for': ''
+        }
+
+        self.issue_with_empty_dates = {
+            'isbn': "123",
+            'username': 'user1',
+            'issue_date': '',
+            'issue_for': ''
+        }
+
+        self.not_found_email_for_reset_password ={
+            'email':'shrooq@gmail.com'
+        }
+        self.not_email_for_reset_password ={
+            'email':'shrooq'
+        }
+        self.weak_password ={
+            'password': '123456789',
+            'password2':'123456789'
+        }
+
+        self.not_matching_passwords ={
+            'password': 'newpassword',
+            'password2': 'newpasswo'
+        }
+        self.empty_passwords ={
+            'password': '',
+            'password2': ''
+        }
+
+        self.empty_password = {
+            'password': '',
+            'password2': 'newpassword'
+        }
+        self.empty_password2 = {
+            'password': 'newpassword',
+            'password2': ''
+        }
+   
         return super().setUp()
 
+
+#*************Register units test****************
 class RegisterTest(BaseTest):
    def test_can_view_page_correctly(self):
        response=self.client.get(self.register_url)
@@ -63,6 +129,7 @@ class RegisterTest(BaseTest):
         response=self.client.post(self.register_url,self.user,format='text/html')
         self.assertEqual(response.status_code,200)
 
+#*************Login units test****************
 class LoginTest(BaseTest):
     def test_can_access_page(self):
         response=self.client.get(self.login_url)
@@ -85,6 +152,78 @@ class LoginTest(BaseTest):
     def test_cantlogin_with_no_username(self):
         response= self.client.post(self.login_url,{'password':'passwped','username':''},format='text/html')
         self.assertEqual(response.status_code,401)
+
     def test_cantlogin_with_no_password(self):
         response= self.client.post(self.login_url,{'username':'passwped','password':''},format='text/html')
         self.assertEqual(response.status_code,401)
+
+#*************Issue units test****************
+class Issue(BaseTest):
+    def test_can_access_page(self):
+        response = self.client.get(self.issue_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'auth/issue.html')
+    
+    def test_not_valid_issue_date(self):
+        response = self.client.post(
+            self.issue_url, self.not_valid_issue_date, format='text/html')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_empty_issue_date(self):
+        response = self.client.post(
+            self.issue_url, self.issue_with_empty_issue_date, format='text/html')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_empty_issue_for(self):
+        response = self.client.post(
+            self.issue_url, self.issue_with_empty_issue_for, format='text/html')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_empty_dates(self):
+        response = self.client.post(
+            self.issue_url, self.issue_with_empty_dates, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+#*************ResetPassword units test****************
+class resetPassword(BaseTest):
+    def test_can_access_page(self):
+        response = self.client.get(self.resetPassword_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'auth/resetPassword.html')
+     
+    def test_empty_passwords(self):
+        response = self.client.post(
+            self.resetPassword_url, self.empty_password2, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_empty_password(self):
+        response = self.client.post(
+            self.resetPassword_url, self.empty_password, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_empty_password2(self):
+        response = self.client.post(
+            self.resetPassword_url, self.empty_password2, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_not_matching_passwords(self):
+        response = self.client.post(
+            self.resetPassword_url, self.not_matching_passwords, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_weak_password(self):
+        response = self.client.post(
+            self.resetPassword_url, self.weak_password, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_not_valid_email(self):
+        response = self.client.post(
+            self.resetPassword_url, self.not_found_email_for_reset_password, format='text/html')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_not_email(self):
+        response = self.client.post(
+            self.resetPassword_url, self.not_email_for_reset_password, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    
--- account/urls.py
+++ account/urls.py
@@ -4,12 +4,12 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
 
-    path('register',views.RegisterationView.as_view() , name = 'register'),
-    path('login',views.loginView.as_view() , name = 'login'),
-    path('home',login_required(views.homeView.as_view() ), name = 'home'),
-    path('logout',views.LogoutView.as_view() , name = 'logout'),
-    path('resetpassword',views.ResstPasswordView.as_view() , name = 'resetpassword'),
-    path('set-new-password/<uidb64>/<token>',views.SetNewPasswordView.as_view(), name='set-new-password'),
+    #path('register',views.RegisterationView.as_view() , name = 'register'),
+    #path('login',views.loginView.as_view() , name = 'login'),
+    #path('home',login_required(views.homeView.as_view() ), name = 'home'),
+    #path('logout',views.LogoutView.as_view() , name = 'logout'),
+    #path('resetpassword',views.ResstPasswordView.as_view() , name = 'resetpassword'),
+    #path('set-new-password/<uidb64>/<token>',views.SetNewPasswordView.as_view(), name='set-new-password'),
 
 
     
