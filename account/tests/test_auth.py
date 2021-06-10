0d8a8c9083a98be54ac51ff51323f23c317d0260
ShrooqAyman <shayman@students.iugaza.edu.ps>
2021-06-10 11:21:55 +0300

    add new units test

--- account/tests/test_auth.py
+++ account/tests/test_auth.py
@@ -13,6 +13,9 @@ class BaseTest(TestCase):
         self.login_url=reverse('login')
         self.issue_url = reverse('issue')
         self.resetPassword_url = reverse('resetPassword')
+        self.changePassword_url = reverse('changePassword')
+        self.addBook_url = reverse('addBook')
+        self.addCategory_url = reverse('addCategory')
         self.user={
             'email':'testemail@gmail.com',
             'username':'username',
@@ -97,7 +100,138 @@ class BaseTest(TestCase):
             'password': 'newpassword',
             'password2': ''
         }
-   
+        self.not_valid_old_password = {
+            'oldPassword': 'oldpasswo',
+            'password': 'newpassword',
+            'password2': 'newpassword'
+        }
+        self.not_valid_new_password = {
+            'oldPassword': 'oldpassword',
+            'password': 'newpa',
+            'password2': 'newpa'
+        }
+        self.not_matching_new_password = {
+            'oldPassword': 'oldpassword',
+            'password': 'newpassword',
+            'password2': 'newpassworh'
+        }
+        self.empty_new_password1 = {
+            'oldPassword': 'oldpassword',
+            'password': '',
+            'password2': 'newpassword'
+        }
+        self.empty_new_password2 = {
+            'oldPassword': 'oldpassword',
+            'password': 'newpassword',
+            'password2': ''
+        }
+        self.empty_new_passwords = {
+            'oldPassword': 'oldpassword',
+            'password': '',
+            'password2': ''
+        }
+        self.empty_new_passwords = {
+            'oldPassword': 'oldpassword',
+            'password': '',
+            'password2': ''
+        }
+
+        self.empty_isbn = {
+            'isbn' :'',
+            'title' :'software',
+            'language' : 'english',
+            'bookPhoto' : 'soft.png',
+            'description' : 'softwre engineering book',
+            'demurage' : '10',
+            'author_name' : 'shrooq',
+            'version_number' : '1',
+            'year' : '2021',
+            'condition' : 'good',
+            'status' : 'delivered'
+
+        }
+
+        self.empty_title = {
+            'isbn': '1234',
+            'title': '',
+            'language': 'english',
+            'bookPhoto': 'soft.png',
+            'description': 'softwre engineering book',
+            'demurage': '10',
+            'author_name': 'shrooq',
+            'version_number': '1',
+            'year': '2021',
+            'condition': 'good',
+            'status': 'delivered'
+
+        }
+
+        self.empty_language = {
+            'isbn': '1234',
+            'title': 'software',
+            'language': '',
+            'bookPhoto': 'soft.png',
+            'description': 'softwre engineering book',
+            'demurage': '10',
+            'author_name': 'shrooq',
+            'version_number': '1',
+            'year': '2021',
+            'condition': 'good',
+            'status': 'delivered'
+
+        }
+        self.empty_author_name = {
+            'isbn': '1234',
+            'title': 'software',
+            'language': 'english',
+            'bookPhoto': 'soft.png',
+            'description': 'softwre engineering book',
+            'demurage': '10',
+            'author_name': ' ',
+            'version_number': '1',
+            'year': '2021',
+            'condition': 'good',
+            'status': 'delivered'
+
+        }
+        self.empty_demurage = {
+            'isbn': '1234',
+            'title': 'software',
+            'language': 'english',
+            'bookPhoto': 'soft.png',
+            'description': 'softwre engineering book',
+            'demurage': '',
+            'author_name': 'shrooq',
+            'version_number': '1',
+            'year': '2021',
+            'condition': 'good',
+            'status': 'delivered'
+
+        }
+        self.not_valid_isbn = {
+            'isbn': 's123',
+            'title': 'software',
+            'language': 'english',
+            'bookPhoto': 'soft.png',
+            'description': 'softwre engineering book',
+            'demurage': '10',
+            'author_name': 'shrooq',
+            'version_number': '1',
+            'year': '2021',
+            'condition': 'good',
+            'status': 'delivered'
+
+        }
+        self.used_cat_name = {
+            'floor_id' :'1',
+            'cat_id' :'2',
+            'cat_name' :'category1' 
+        }
+        self.empty_cat_name = {
+            'floor_id': '1',
+            'cat_id': '2',
+            'cat_name': ''
+        }
         return super().setUp()
 
 
@@ -226,4 +360,92 @@ class resetPassword(BaseTest):
             self.resetPassword_url, self.not_email_for_reset_password, format='text/html')
         self.assertEqual(response.status_code, 200)
 
-    
+#*************changePassword units test****************
+class changePassword(BaseTest):
+    def test_can_access_page(self):
+        response = self.client.get(self.changePassword_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'auth/changePassword.html')
+
+    def test_empty_new_passwords(self):
+        response = self.client.post(
+            self.changePassword_url, self.empty_new_passwords, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_empty_new_password(self):
+        response = self.client.post(
+            self.changePassword_url, self.empty_new_password1, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_empty_new_password2(self):
+        response = self.client.post(
+            self.changePassword_url, self.empty_new_password2, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_not_matching_passwords(self):
+        response = self.client.post(
+            self.changePassword_url, self.not_matching_new_password, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_not_valid_new_password(self):
+        response = self.client.post(
+            self.changePassword_url, self.not_valid_new_password, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_not_valid_old_password(self):
+        response = self.client.post(
+            self.changePassword_url, self.not_valid_old_password, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+#*************add book units test****************
+class addBook(BaseTest):
+    def test_can_access_page(self):
+        response = self.client.get(self.addBook_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'auth/addBook.html')
+
+    def test_empty_isbn(self):
+        response = self.client.post(
+            self.addBook_url, self.empty_isbn, format='text/html')
+        self.assertEqual(response.status_code, 200)
+    def test_empty_title(self):
+        response = self.client.post(
+            self.addBook_url, self.empty_title, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_empty_author(self):
+        response = self.client.post(
+            self.addBook_url, self.empty_author_name, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_empty_language(self):
+        response = self.client.post(
+            self.addBook_url, self.empty_language, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_empty_demurage(self):
+        response = self.client.post(
+            self.addBook_url, self.empty_demurage, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_not_valid_isbn(self):
+        response = self.client.post(
+            self.addBook_url, self.not_valid_isbn, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+#*************add Category units test****************
+class addCategory(BaseTest):
+    def test_can_access_page(self):
+        response = self.client.get(self.addCategory_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'auth/addCategory.html')
+
+    def test_empty_cat_name(self):
+        response = self.client.post(
+            self.addCategory_url, self.empty_cat_name, format='text/html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_used_cat_name(self):
+        response = self.client.post(
+            self.addCategory_url, self.used_cat_name, format='text/html')
+        self.assertEqual(response.status_code, 200)
